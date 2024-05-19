e10f24e5e107bc658ac74ae55a2299b3a94dd5df
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-19 10:29:18 +0530

    pushed by sundarraj

---
+++ src/com/reversepattern/ReversePattern.java
@@ -0,0 +1,13 @@
+package com.reversepattern;
+
+public class ReversePattern {
+	public static void main(String[] args) {
+		int a = 3;
+		for(int i =a; i>=0; i--) {
+			for(int j = 1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+}
---
+++ src/pattern/Pattern.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		int a = 3;
+		for(int i =1; i<=a; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
